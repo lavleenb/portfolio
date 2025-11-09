782b32fbc70f46e555be250d3308666d8be6eb6a
lavleen <lavleenbhachu4@gmail.com>
2025-11-09 10:08:02 -0500

    art gallery construction

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ $(document).ready(function() {
     $(this).toggleClass('slide-up');
   });
 });
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+$(document).ready(function() {
+
+  $(".popup_image").on('click', function() {
+    w2popup.open({
+      title: 'Image',
+      body: '<div class="w2ui-centered"><img src="' + $(this).attr('src') + '"></img></div>'
+    });
+  });
+
+});
